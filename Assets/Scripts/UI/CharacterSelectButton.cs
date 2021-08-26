79a745b3d6434d4020a54cd7f3edab68b8afd425
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-08-25 21:07:09 -0700

    updated character avatar image color when selected

--- Assets/Scripts/UI/CharacterSelectButton.cs
+++ Assets/Scripts/UI/CharacterSelectButton.cs
@@ -1,4 +1,5 @@
 using PropHunt.Character.Avatar;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,8 @@ namespace PropHunt.UI
         /// </summary>
         public const string CharacterSelectPerfKey = "CharacterSelected";
 
+        public static EventHandler NewCharacterSelected;
+
         /// <summary>
         /// String which contains the avatar ID associated with this button
         /// </summary>
@@ -27,13 +30,31 @@ namespace PropHunt.UI
         public void Start()
         {
             Toggle toggle = GetComponent<Toggle>();
-            toggle.onValueChanged.AddListener(value => 
+            Image avatarImage = GetComponent<Image>();
+            toggle.onValueChanged.AddListener(value =>
             {
                 if (value)
                 {
                     UpdateAvatar();
                 }
             });
+            NewCharacterSelected += (sender, args) => UpdateImageState(toggle, avatarImage);
+            UpdateImageState(toggle, avatarImage);
+        }
+
+        public void UpdateImageState(Toggle toggle, Image avatarImage)
+        {
+            toggle.SetIsOnWithoutNotify(CharacterAvatarManager.defaultAvatar == avatarId);
+            // If our avatar is selected
+            if (CharacterAvatarManager.defaultAvatar == avatarId)
+            {
+                avatarImage.color = Color.cyan;
+            }
+            // If our avatar is de-selected
+            else
+            {
+                avatarImage.color = Color.white;
+            }
         }
 
         /// <summary>
@@ -49,6 +70,8 @@ namespace PropHunt.UI
                 .Where(avatar => avatar.isLocalPlayer)
                 .ToList()
                 .ForEach(avatar => avatar.CmdSetAvatar(avatarId));
+
+            NewCharacterSelected?.Invoke(this, EventArgs.Empty);
 
             // Update default avatar in perf key
             PlayerPrefs.SetString(CharacterSelectPerfKey, avatarId);
