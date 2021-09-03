32c1579e48c7000b30a92a3b016ec55fb50c8312
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-09-02 21:36:54 -0700

    Fixed arms to have flat bottom so they sweep plaeyr off the edge

--- Assets/Scripts/Environment/RotateAboutPoint.cs
+++ Assets/Scripts/Environment/RotateAboutPoint.cs
@@ -17,8 +17,30 @@ namespace PropHunt.Environment
         [Tooltip("Angular velocity of object in degrees per second for each euclidian axis")]
         protected Vector3 angularVelocity;
 
+        /// <summary>
+        /// Accelerating the rigidbody in degrees per second squared
+        /// </summary>
+        [SerializeField]
+        [SyncVar]
+        [Tooltip("Acceleration of rigidbody c squared")]
+        protected Vector3 angularAcceleration;
+
+        /// <summary>
+        /// maximum angular velocity in each axis in degrees per second
+        /// </summary>
+        [SerializeField]
+        [SyncVar]
+        [Tooltip("Maximum angular velocity in each axis in degrees per second")]
+        protected Vector3 maxAngularVelocity;
+
+        /// <summary>
+        /// Position of the object.
+        /// </summary>
         protected Vector3 pos;
 
+        /// <summary>
+        /// Rigidbody attached to the object
+        /// </summary>
         protected Rigidbody objRigidbody;
 
         public void Start()
@@ -29,6 +51,11 @@ namespace PropHunt.Environment
 
         public void Update()
         {
+            angularVelocity += angularAcceleration * Time.deltaTime;
+            angularVelocity = new Vector3(
+                Mathf.Min(angularVelocity.x, maxAngularVelocity.x),
+                Mathf.Min(angularVelocity.y, maxAngularVelocity.y),
+                Mathf.Min(angularVelocity.z, maxAngularVelocity.z));
             transform.position = pos;
             objRigidbody.centerOfMass = Vector3.zero;
             objRigidbody.velocity = Vector3.zero;
