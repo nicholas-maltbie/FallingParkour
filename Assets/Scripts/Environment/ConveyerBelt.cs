ef221972bf742865557d816f066ca56337fb6dc2
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-09-08 19:14:38 -0700

    Added conveyer belt that is 16 units long

--- Assets/Scripts/Environment/ConveyerBelt.cs
+++ Assets/Scripts/Environment/ConveyerBelt.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 namespace PropHunt.Environment
 {
+    public enum RelativeDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        Forward,
+        Backward
+    }
+
     /// <summary>
     /// Conveyer belt that has a uniform direction and speed
     /// </summary>
@@ -11,16 +21,41 @@ namespace PropHunt.Environment
         private bool avoidTransferMomentum;
 
         [SerializeField]
-        private Vector3 velocity;
+        private float velocity;
+
+        [SerializeField]
+        private RelativeDirection direction = RelativeDirection.Down;
+
+        public Vector3 GetDirection()
+        {
+            switch (this.direction)
+            {
+                case RelativeDirection.Up:
+                    return transform.up;
+                case RelativeDirection.Down:
+                    return -transform.up;
+                case RelativeDirection.Left:
+                    return -transform.right;
+                case RelativeDirection.Right:
+                    return transform.right;
+                case RelativeDirection.Forward:
+                    return transform.forward;
+                case RelativeDirection.Backward:
+                    return -transform.forward;
+            }
+            return transform.forward;
+        }
 
         /// <inheritdoc/>
         public bool AvoidTransferMomentum() => avoidTransferMomentum;
 
         /// <inheritdoc/>
-        public Vector3 GetDisplacementAtPoint(Vector3 point, float deltaTime) => velocity * deltaTime;
+        public Vector3 GetDisplacementAtPoint(Vector3 point, float deltaTime) =>
+            velocity * GetDirection() * deltaTime;
 
         /// <inheritdoc/>
-        public Vector3 GetVelocityAtPoint(Vector3 point, float deltaTime) => velocity;
+        public Vector3 GetVelocityAtPoint(Vector3 point, float deltaTime) =>
+            velocity * GetDirection();
 
         /// <inheritdoc/>
         public bool ShouldAttach() => false;
