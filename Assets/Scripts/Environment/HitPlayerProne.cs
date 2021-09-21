9c2d4dc847d7bf79b95533671d7f24ff9491bc0c
Nicholas Maltbie <nick.dmalt@gmail.com>
2021-09-21 01:09:26 -0700

    Finished initial version of level

--- Assets/Scripts/Environment/HitPlayerProne.cs
+++ Assets/Scripts/Environment/HitPlayerProne.cs
@@ -10,11 +10,18 @@ namespace PropHunt.Environment
     public class HitPlayerProne : MonoBehaviour
     {
         /// <summary>
-        /// How long should a player be knocked prone (minimum time) after they are hit?
+        /// How long should a player be knocked prone (maximum time) after they are hit?
         /// </summary>
         [SerializeField]
         [Tooltip("Time to set player prone when hit by this.")]
-        private float proneTime = 3.0f;
+        private float maxProneTime = 3.0f;
+
+        /// <summary>
+        /// How long should a player be knocked prone (minimum time) after they are hit?
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Minimum time player is prone after being hit.")]
+        private float minProneTime = 0.1f;
 
         public void OnCollisionEnter(Collision collision)
         {
@@ -35,9 +42,9 @@ namespace PropHunt.Environment
             }
 
             // Knock the player prone for prone time seconds
-            kcc.KnockPlayerProne(proneTime);
+            kcc.KnockPlayerProne(minProneTime, maxProneTime);
             Vector3 point = collision.contacts[0].point;
-            collision.rigidbody.AddForceAtPosition(GetComponent<Rigidbody>().GetPointVelocity(point) * forceMod, point);
+            collision.rigidbody.AddForceAtPosition(GetComponent<Rigidbody>().GetPointVelocity(point) * forceMod, point, ForceMode.Impulse);
         }
     }
 }
